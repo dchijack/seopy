14eab191077f606e53ac5b65df13d1a858c6178c
Vicente Gutiérrez <gtz.rvera@gmail.com>
2017-06-07 22:46:46 -0500

    Se creó el método para obtener los meta tags del sitio web Se creó además el método analyse que será el encargado de llamar a cada uno de los métodos para obtener información específica del sitio web.

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ if __name__ == '__main__':
     url = 'http://www.informador.com.mx'
     
     seo_analyst = SEOAnalyst(url)
+    seo_analyst.analyse()
+    
     print(seo_analyst.url)
+    print(seo_analyst.meta_tags)
--- seopy/seo_analyst.py
+++ seopy/seo_analyst.py
@@ -7,3 +7,32 @@ class SEOAnalyst:
 
     def __init__(self, url):
         self.url = url
+
+
+    def analyse(self):
+        request = requests.get(self.url)
+        html_site = BeautifulSoup(request.text, 'html.parser')
+        
+        # Get meta tags
+        self.meta_tags = self.get_meta_tags(html_site)
+
+
+    def get_meta_tags(self, html_site):
+        tags = html_site.find_all('meta')
+        meta_tags = {'facebook': [], 'twitter': [], 'meta_tags': []}
+
+        for tag in tags:
+
+            if 'property' in tag.attrs:
+                tag_info = {'property': tag.attrs['property'], 'content': tag.attrs['content']}
+                meta_tags['facebook'].append(tag_info)
+            else:
+                if 'name' in tag.attrs:
+                    tag_info = {'name': tag.attrs['name'], 'content': tag.attrs['content']}
+
+                    if tag.attrs['name'].startswith('twitter'):
+                        meta_tags['twitter'].append(tag_info)
+                    else:
+                        meta_tags['meta_tags'].append(tag_info)
+
+        return meta_tags

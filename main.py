095cf0d0cbdad71ed0775967c172d37a19acb85f
Vicente Gutiérrez <gtz.rvera@gmail.com>
2017-07-01 19:12:52 -0500

    Modificaciones con fines de pruebas para la clase SEOAnalyst

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ if __name__ == '__main__':
     url = 'http://www.informador.com.mx'
     
     seo_analyst = SEOAnalyst(url)
-    print(seo_analyst.url)
+    seo_analyst.analyse()
+    
+
+    # print(seo_analyst.url)
+    # print(seo_analyst.meta_tags)
+    # print(seo_analyst.title)
+    # print(seo_analyst.headers)
+    # print(seo_analyst.images)
+    # print(seo_analyst.frames)
+    print(seo_analyst.links)
